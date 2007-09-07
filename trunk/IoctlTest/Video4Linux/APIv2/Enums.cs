1e7f43f4af1540c865562fc805c7bf610b5158db
twenty-three <twenty-three@e3ab30c2-4239-0410-a2c8-8df5c1906d62>
2007-09-07 11:42:56 +0000

    added the reserved image formats

--- trunk/IoctlTest/Video4Linux/APIv2/Enums.cs
+++ trunk/IoctlTest/Video4Linux/APIv2/Enums.cs
@@ -188,7 +188,7 @@ namespace Video4Linux.APIv2
 		BGR24   =  861030210, // BGR3
 		RGB24   =  859981650, // RGB3
 		BGR32   =  877807426, // BGR4
-		RGB32   =  876758866,  // RGB4
+		RGB32   =  876758866, // RGB4
 		
 		// YUV formats
 		GREY    = 1497715271, // GREY
@@ -201,7 +201,19 @@ namespace Video4Linux.APIv2
 		YUV410  =  961959257, // YUV9
 		YUV422P = 1345466932, // 422P
 		NV12    =  842094158, // NV12
-		NV21    =  825382478 // NV21
+		NV21    =  825382478, // NV21
+		
+		// Reserved
+		DV       = 1685288548, // dvsd
+		ET61X251 =  892483141, // E625
+		HI240    =  875710792, // HI24
+		HM12     =  842091848, // HM12
+		MJPEG    = 1196444237, // MJPG
+		PWC1     =  826496848, // PWC1
+		PWC2     =  843274064, // PWC2
+		SN9C10X  =  808532307, // S910
+		WNVA     = 1096175191, // WNVA
+		YYUV     = 1448434009  // YYUV
 	}
 	
 	class keks
